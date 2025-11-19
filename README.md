aa63cc1fb8d914dcec884c176e420e0b7b78267b
kaspar.kipp <kipp@ut.ee>
2025-11-20 00:55:02 +0200

    SSL in dev

--- README.md
+++ README.md
@@ -9,7 +9,9 @@
 * Run `mix setup` to install and setup dependencies
 * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix`
 
-Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+Now you can visit [`https://localhost:4001`](https://localhost:4001) from your browser.
+> Note: HTTPS is required to communicate with the Web eID browser extension
+
 
 ---------
 
--- config/dev.exs
+++ config/dev.exs
@@ -11,6 +11,12 @@ config :app, AppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  https: [
+    port: String.to_integer(System.get_env("HTTPS_PORT") || "4001"),
+    cipher_suite: :strong,
+    keyfile: "priv/cert/selfsigned_key.pem",
+    certfile: "priv/cert/selfsigned.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
